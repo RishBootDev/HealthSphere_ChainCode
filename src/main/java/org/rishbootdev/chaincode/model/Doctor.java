854b699e7a2438ae692fa70835520c7d80cce443
Rishabh <rishabhdubey1472004@gmail.com>
2025-11-01 13:37:26 +0530

    worked on the model classes

--- src/main/java/org/rishbootdev/chaincode/model/Doctor.java
+++ src/main/java/org/rishbootdev/chaincode/model/Doctor.java
@@ -7,7 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hyperledger.fabric.contract.annotation.DataType;
 import org.hyperledger.fabric.contract.annotation.Property;
-
 import java.util.ArrayList;
 import java.util.List;
 
